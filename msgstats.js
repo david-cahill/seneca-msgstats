c64c050704d441c31e88cbb97aa20a2b9318a832
david-cahill <davidrichardcahill@gmail.com>
2015-01-14 16:17:35 +0000

    fix order of seneca patterns

--- msgstats.js
+++ msgstats.js
@@ -55,7 +55,27 @@ module.exports = function( options ) {
 
   function getSenecaPatterns(cb) {
     var patterns = seneca.list();
-    cb(null, patterns);
+    seneca.act({role:'collector', cmd:'getSenecaActions'}, function(err,result) {
+      cb(null, result);
+    });
+  }
+
+  function sortObject(o) {
+    var sorted = {},
+    key, a = [];
+
+    for (key in o) {
+      if (o.hasOwnProperty(key)) {
+        a.push(key);
+      }
+    }
+
+    a.sort();
+
+    for (key = 0; key < a.length; key++) {
+      sorted[a[key]] = o[a[key]];
+    }
+    return sorted;
   }
 
   function parseSenecaPatterns(patterns, searchTerm, cb) {
